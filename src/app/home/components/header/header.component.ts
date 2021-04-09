084c62f4d14180a6e87c0f11e526e99bff5aa5d8
ericho613 <eric.ho@csc-scc.gc.ca>
2021-04-09 13:16:58 -0400

    Added formatting for download data chunks.

--- src/app/home/components/header/header.component.ts
+++ src/app/home/components/header/header.component.ts
@@ -36,13 +36,25 @@ export class HeaderComponent implements OnInit, OnDestroy {
       if(this.showInstallAndRestartBanner){
         this.showDownloadProgressBanner = false;
       };
-      this.progressTotal = headerState.progressTotal + "";
-      this.progressTransferred = headerState.progressTransferred + "";
+      this.progressTotal = this.formatBytes(headerState.progressTotal);
+      this.progressTransferred = this.formatBytes(headerState.progressTransferred);
       this.progressPercent = Math.round(headerState.progressPercent) + "";
 
       this.changeRef.detectChanges()
     });
 
+  }
+
+  formatBytes(bytes, decimals = 2) {
+    if (bytes === 0) return '0 Bytes';
+  
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+  
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
   }
 
   downloadUpdate(){
